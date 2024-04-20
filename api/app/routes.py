e601e0d4926572b61182b9f2e29de5ffb434d96a
AndzhurT <andjur38@gmail.com>
2024-04-20 15:58:17 -0400

    backend post commit

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,5 +1,19 @@
 from app import app
+from flask import request, jsonify, render_template
 
 @app.route('/')
 def home():
     return "Welcome to the Flask API!"
+
+@app.route('/profile/<name>')
+def profile(name):
+    return render_template("index.html", name=name)
+
+@app.route('/receive_message', methods=['POST'])
+def receive_message():
+    data = request.json  # Expecting JSON data
+    user_message = data['message']
+    print("Received message from user:", user_message)  # Log the message to console
+    return jsonify({"status": "Message received", "your_message": user_message})
+
+
