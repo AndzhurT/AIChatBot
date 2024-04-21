c09fdef81d99957aa64a229f0405a9a838683008
AndzhurT <andjur38@gmail.com>
2024-04-20 20:19:25 -0400

    chatbot commit

--- api/app/routes.py
+++ api/app/routes.py
@@ -10,8 +10,16 @@ def home():
 def ask():
     data = request.json  # Expecting JSON data
     user_message = data['message']
+    bot_response = bot.get_response(user_message)
+
+
     print("Received message from user:", user_message)  # Log the message to console
-    print(str(bot.get_response(user_message)))
-    return str(bot.get_response(user_message))
+    print(str(bot_response))
+    print(bot_response.confidence)
+    if bot_response.confidence:
+            bot_response = str(bot_response)      
+            print(bot_response)
+            return str(bot.get_response(user_message))
+    return "I don't know what you are asking"
 
 
